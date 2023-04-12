4baf2179e4e7c013d94fc1b7a50daafbc33149c1
Ycow <skypnal12@gmail.com>
2023-04-13 03:49:52 +0900

    feat: router 디테일 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './page/Home/Home';
+import MusicDetail from './page/MusicDetail/MusicDetail';
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/music/:id" element={<MusicDetail />} />
       </Routes>
     </BrowserRouter>
   );
