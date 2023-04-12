874177233ee7a2e687d6e3130e68ff1756451d52
Ycow <skypnal12@gmail.com>
2023-04-13 03:53:24 +0900

    feat: top100 리스트 컴포넌트 추가

---
+++ src/components/MusicTop100List/MusicTop100List.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { MusicListTypes } from '../../types/types';
+import styles from './MusicTop100List.module.css';
+import highlightedText from '../../lib/util/highlightedText';
+
+interface Props {
+  data: MusicListTypes[] | undefined;
+  queryText: string;
+}
+
+function MusicTop100List({ data, queryText }: Props) {
+  return (
+    <>
+      <ul className={styles.musicList}>
+        {data?.map((el, idx) => (
+          <li key={el.id.label} className={styles.musicItem}>
+            <Link to={`/music/${el.id.attributes['im:id']}`} className={styles.musicLink}>
+              <div className={styles.ranking}>{idx + 1}</div>
+              <div className={styles.albumImg}>
+                <img src={el['im:image'][1].label} alt="앨범 이미지" />
+              </div>
+              <div className={styles.albumTitle}>
+                {highlightedText({ text: el['im:name'].label, query: queryText })}
+              </div>
+              <div className={styles.artistName}>{el['im:artist'].label}</div>
+              <div className={styles.price}>{el['im:price'].label}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default MusicTop100List;
